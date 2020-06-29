4d1b99c49ead752331f1a428f909ed614751c716
Noa Kim <noanonoa@gmail.com>
2020-06-29 11:53:17 -0700

    added NPM pkgs for middleware

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ require('dotenv').config();
 //REQUIRE express AND SETUP AND express app INSTANCE
 const Express = require('express');
 //REQUIRE AND SET view engine USE ejs
-const ejsLayouts = require('express-ejs-layouts')
-//SET app TO USE false urlencoding
-//SET app public directory FOR USE
-//SET app ejs layouts FOR RENDER
+const ejsLayouts = require('express-ejs-layouts');
+//REQUIRE ALL MIDDLEWARE FOR APP/AUTHENTICATION
+// helmet, morgan, passport, AND CUSTOM MIDDLEWARE, express-sessions, sequelize sessions, flash
+const helmet = require('helmet');
+const session = require('express-session');
+const flash = require('flash');
 
 
 //APP SETUP
 const app = Express();
+//SET app TO USE false urlencoding
 app.use(Express.urlencoded({ extended: false }));
+//SET app public directory FOR USE
 app.use(Express.static(__dirname + '/public'));
+//SET app ejs layouts FOR RENDER
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(require('morgan')('dev'));
+app.use(helmet());
+
 
 
 //ROUTES
